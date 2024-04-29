564fc13a03fba85ca38139bfc1643afab25aeeef
cron410 <3082899+cron410@users.noreply.github.com>
2024-04-29 10:27:45 -0400

    Update main.tf
    Add null_resource to trigger data regenerate for every run. Normally one would not need to regenerate data for every terraform run when interacting manually, this takes a long time. The depends_on line can be commented out in that case. If this code is run in a pipeline on a schedule once or twice a week, leave it uncommented.

--- main.tf
+++ main.tf
@@ -13,24 +13,77 @@ provider "aws" {
   profile = var.profile
 }
 
-data "aws_instances" "instances_list" {
-  instance_tags = {
-    (var.tag_name) = var.tag_value
+locals {
+  nonprod_instance_ids = [for InstanceId in local.nonprod_disk_paths.Dimensions : InstanceId.InstanceId]
+  nonprod_disk_paths   = jsondecode(file("ec2_alarm/nonprod_disk_paths.json"))
+  nonprod_disk_path    = [for path in local.nonprod_disk_paths.Dimensions : path.path]
+  prod_instance_ids    = [for InstanceId in local.prod_disk_paths.Dimensions : InstanceId.InstanceId]
+  prod_disk_paths      = jsondecode(file("ec2_alarm/prod_disk_paths.json"))
+  prod_disk_path       = [for path in local.prod_disk_paths.Dimensions : path.path]
+  #  instance_id = [for InstanceId in data.external.disks.result.Dimensions : InstanceId.InstanceId]
+  #  disk_path = [for path in data.external.disks.result.Dimensions : path.path]
+  #  #change the keys on the instance list so we can find them by name
+  #  nonprod_instance_sanitized = {
+  #    for key, value in data.aws_instance.nonprod_instance :
+  #      value.tags.Name => value
+  #  }
+  #  nonprod_instance_disks = distinct(flatten([
+  #    for nonprod_instance in local.nonprod_instance_sanitized : [
+  #      for disk_path in local.disk_paths : {
+  #        nonprod_instance = nonprod_instance
+  #        disk_path = disk_path
+  #      }
+  #    ]
+  #  ]))
+}
+
+module "nonprod_ec2_alarms" {
+  count       = length(local.nonprod_instance_ids)
+  source      = "./ec2_alarm"
+  instance_id = local.nonprod_instance_ids[count.index]
+  region      = var.region
+  profile     = var.profile
+  # tag_name           = var.tag_name
+  # tag_value          = var.tag_value
+  disk_path          = local.nonprod_disk_path[count.index]
+  threshold_ec2_disk = var.threshold_ec2_disk
+  threshold_ec2_cpu  = var.threshold_ec2_cpu
+  threshold_ec2_mem  = var.threshold_ec2_mem
+  alarm_actions      = var.sns_topics_nonprod
+  ok_actions         = var.sns_topics_nonprod
+  actions_enabled    = true
+  depends_on         = [null_resource.aws_temp_data_regenerate]
+}
+
+module "prod_ec2_alarms" {
+  count       = length(local.prod_instance_ids)
+  source      = "./ec2_alarm"
+  instance_id = local.prod_instance_ids[count.index]
+  region      = var.region
+  profile     = var.profile
+  # tag_name           = var.tag_name
+  # tag_value          = var.tag_value
+  disk_path          = local.prod_disk_path[count.index]
+  threshold_ec2_disk = var.threshold_ec2_disk
+  threshold_ec2_cpu  = var.threshold_ec2_cpu
+  threshold_ec2_mem  = var.threshold_ec2_mem
+  alarm_actions      = var.sns_topics_prod
+  ok_actions         = var.sns_topics_prod
+  actions_enabled    = true
+  depends_on         = [null_resource.aws_temp_data_regenerate]
+}
+
+resource "null_resource" "aws_temp_data_regenerate" {
+  provisioner "local-exec" {
+    command     = "bash instances.sh"
+    working_dir = "./ec2_alarm"
+    environment = {
+      PROFILE       = var.profile
+      REGION        = var.region
+      OUTPUT_FOLDER = "ec2_alarm"
+    }
+  }
+  triggers = {
+    always_run = timestamp()
   }
 }
-
-module instance_names {
-  for_each    = toset(data.aws_instances.instances_list.ids)
-  source      = "./ec2_alarm"
-  instance_id = each.key
-
-  region             = var.region
-  profile            = var.profile
-  tag_name           = var.tag_name
-  tag_value          = var.tag_value
-  threshold_ec2_cpu  = var.threshold_ec2_cpu
-  threshold_ec2_disk = var.threshold_ec2_disk
-  threshold_ec2_mem  = var.threshold_ec2_mem
-  alarm_actions      = var.alarm_actions
-}
-
