e84755758bf0f8f5575d5d181c8a927f316893bc
cron410 <3082899+cron410@users.noreply.github.com>
2024-04-29 10:19:26 -0400

    Update main.tf
    Use disk dimensions directly from data.external
    
    Use variables from module instead of arguments from command line.

--- ec2_alarm/main.tf
+++ ec2_alarm/main.tf
@@ -1,85 +1,42 @@
-variable instance_id {}
-variable tag_name {}
-variable tag_value {}
-variable threshold_ec2_cpu {}
-variable threshold_ec2_disk {}
-variable threshold_ec2_mem {}
-variable alarm_actions {}
-variable region {}
-variable profile {}
+terraform {
+  required_version = ">=1.0.11"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">=4.11"
+    }
+    external = {
+      source  = "hashicorp/external"
+      version = "~> 2.2.3"
+    }
+  }
+}
+
+data "external" "disk_dimensions" {
+  program     = ["bash", "disk_dimensions.sh"]
+  working_dir = path.module
+  query = {
+    profile     = var.profile
+    region      = var.region
+    instance_id = var.instance_id
+    disk_path   = var.disk_path
+#   disk_path   = local.disk_paths[count.index]
+  }
+}
 
 data "aws_instance" "each_instance" {
   instance_id = var.instance_id
 }
 
-data "external" "disk_dimensions" {
-  program = ["bash", "${path.module}/disk_dimensions.sh"]
-  query = {
-    profile = var.profile
-    region = var.region
-    instance_id = var.instance_id
-  }
-}
-
-resource "aws_cloudwatch_metric_alarm" "cpu_utilization_alarm" {
-  alarm_name = join("|", [
-    "Alarm", 
-    var.region, 
-    "EC2", 
-    data.aws_instance.each_instance.tags.Name,
-    "CPUUtilization"
-  ])
-
-  comparison_operator       = "GreaterThanOrEqualToThreshold"
-  evaluation_periods        = "1"
-  metric_name               = "CPUUtilization"
-  namespace                 = "AWS/EC2"
-  period                    = "300"
-  statistic                 = "Average"
-  threshold                 = var.threshold_ec2_cpu
-  alarm_description         = "This metric monitors ec2 cpu utilization"
-  insufficient_data_actions = []
-  alarm_actions             = var.alarm_actions
-
-  dimensions = {
-    InstanceId = var.instance_id
-  }
-}
-
-resource "aws_cloudwatch_metric_alarm" "mem_used_percent_alarm" {
-  alarm_name = join("|", [
-    "Alarm", 
-    var.region, 
-    "EC2", 
-    data.aws_instance.each_instance.tags.Name,
-    "mem_used_percent"
-  ])
-
-  comparison_operator       = "GreaterThanOrEqualToThreshold"
-  evaluation_periods        = "1"
-  metric_name               = "mem_used_percent"
-  namespace                 = "CWAgent"
-  period                    = "300"
-  statistic                 = "Average"
-  threshold                 = var.threshold_ec2_mem
-  alarm_description         = "This metric monitors ec2 memory utilization"
-  insufficient_data_actions = []
-  alarm_actions             = var.alarm_actions
-
-  dimensions = {
-    InstanceId = var.instance_id
-    ImageId = data.aws_instance.each_instance.ami
-    InstanceType = data.aws_instance.each_instance.instance_type
-  }
-}
-
 resource "aws_cloudwatch_metric_alarm" "disk_used_percent_alarm" {
-  alarm_name = join("|", [
-    "Alarm", 
-    var.region, 
-    "EC2", 
+  #count = length(local.disk_paths)
+
+  alarm_name = join(" - ", [
+    "DiskSpace",
+    data.aws_instance.each_instance.tags.Environment,
+    var.disk_path,
     data.aws_instance.each_instance.tags.Name,
-    "disk_used_percent"
+    var.region
   ])
 
   comparison_operator       = "GreaterThanOrEqualToThreshold"
@@ -91,14 +48,114 @@ resource "aws_cloudwatch_metric_alarm" "disk_used_percent_alarm" {
   threshold                 = var.threshold_ec2_disk
   alarm_description         = "This metric monitors ec2 disk utilization"
   insufficient_data_actions = []
+  treat_missing_data        = "ignore"
   alarm_actions             = var.alarm_actions
+  ok_actions                = var.ok_actions
+  actions_enabled           = var.actions_enabled
 
+  dimensions = data.external.disk_dimensions.result
+  #  dimensions = data.external.disk_dimensions[count.index].result
+  #  dimensions = {
+  #    InstanceId = data.external.disk_dimensions.result.InstanceId
+  #    path = data.external.disk_dimensions.result.path
+  #    ImageId = data.external.disk_dimensions.result.ImageId
+  #    InstanceType = data.external.disk_dimensions.result.InstanceType
+  #    device = data.external.disk_dimensions.result.device
+  #    fstype = data.external.disk_dimensions.result.fstype
+  #  }
+  tags = {
+    Engagement  = tostring(data.aws_instance.each_instance.tags.Engagement)
+    Environment = data.aws_instance.each_instance.tags.Environment
+    MGMT        = "Managed by Terraform"
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "cpu_utilization" {
+  alarm_name = join(" - ", [
+    "CPU Used",
+    data.aws_instance.each_instance.tags.Name,
+    var.region
+  ])
+  alarm_description         = "Average ec2 instance CPU utilization"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = "1"
+  metric_name               = "CPUUtilization"
+  namespace                 = "AWS/EC2"
+  period                    = "300"
+  statistic                 = "Average"
+  threshold                 = var.threshold_ec2_cpu
+  insufficient_data_actions = []
+  treat_missing_data        = "ignore"
+  alarm_actions             = var.alarm_actions
+  ok_actions                = var.ok_actions
+  actions_enabled           = var.actions_enabled
   dimensions = {
-    InstanceId = data.external.disk_dimensions.result.InstanceId
-    path = data.external.disk_dimensions.result.path
-    ImageId = data.external.disk_dimensions.result.ImageId
-    InstanceType = data.external.disk_dimensions.result.InstanceType
-    device = data.external.disk_dimensions.result.device
-    fstype = data.external.disk_dimensions.result.fstype
+    InstanceId = var.instance_id
+  }
+  tags = {
+    Engagement  = tostring(data.aws_instance.each_instance.tags.Engagement)
+    Environment = data.aws_instance.each_instance.tags.Environment
+    MGMT        = "Managed by Terraform"
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "mem_used_percent_alarm" {
+  alarm_name = join(" - ", [
+    "Memory Used",
+    data.aws_instance.each_instance.tags.Name,
+    var.region
+  ])
+  alarm_description         = "This metric monitors ec2 memory utilization"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = "1"
+  metric_name               = "mem_used_percent"
+  namespace                 = "CWAgent"
+  period                    = "300"
+  statistic                 = "Average"
+  threshold                 = var.threshold_ec2_mem
+  insufficient_data_actions = []
+  treat_missing_data        = "ignore"
+  alarm_actions             = var.alarm_actions
+  ok_actions                = var.ok_actions
+  actions_enabled           = var.actions_enabled
+  dimensions = {
+    InstanceId   = var.instance_id
+    ImageId      = data.aws_instance.each_instance.ami
+    InstanceType = data.aws_instance.each_instance.instance_type
+  }
+  tags = {
+    Engagement  = tostring(data.aws_instance.each_instance.tags.Engagement)
+    Environment = data.aws_instance.each_instance.tags.Environment
+    MGMT        = "Managed by Terraform"
+  }
+}
+
+
+resource "aws_cloudwatch_metric_alarm" "status-check-failed" {
+  alarm_name = join(" - ", [
+    "Status Check Failed",
+    data.aws_instance.each_instance.tags.Name,
+    var.region
+  ])
+  alarm_description         = "EC2 Status Check Failed"
+  comparison_operator       = "GreaterThanThreshold"
+  evaluation_periods        = "1"
+  metric_name               = "StatusCheckFailed"
+  namespace                 = "AWS/EC2"
+  period                    = "300"
+  statistic                 = "Average"
+  threshold                 = "0.0"
+  insufficient_data_actions = []
+  treat_missing_data        = "ignore"
+  alarm_actions             = var.alarm_actions
+  ok_actions                = var.ok_actions
+  actions_enabled           = var.actions_enabled
+  dimensions = {
+    InstanceId = var.instance_id
+  }
+  tags = {
+    Engagement  = tostring(data.aws_instance.each_instance.tags.Engagement)
+    Environment = data.aws_instance.each_instance.tags.Environment
+    MGMT        = "Managed by Terraform"
   }
 }
