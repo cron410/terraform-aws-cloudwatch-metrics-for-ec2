79e11dac022cd90217a2ecf633002da6c9af0819
cron410 <3082899+cron410@users.noreply.github.com>
2024-04-29 10:23:40 -0400

    Create output.tf
    Example outputs, not currently used in my projects.

---
+++ ec2_alarm/output.tf
@@ -0,0 +1,19 @@
+#output "disk_dimensions_path" {
+#  description = "disk path from dimensions script"
+#  value       = data.external.disk_dimensions.result.path
+#}
+#
+#output "disk_dimensions_instanceid" {
+#  description = "instanceid from dimensions script"
+#  value       = data.external.disk_dimensions.result.InstanceId
+#}
+#
+#output "disk_dimensions_device" {
+#  description = "device path from dimensions script"
+#  value       = data.external.disk_dimensions.result.device
+#}
+#
+#output "disk_dimensions" {
+#  description = "device path from dimensions script"
+#  value       = data.external.disk_dimensions.result
+#}
