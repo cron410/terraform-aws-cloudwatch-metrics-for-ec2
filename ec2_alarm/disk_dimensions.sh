8aae1770fc01ab415fdf3b9ad9cea3d0fdb4d83c
cron410 <3082899+cron410@users.noreply.github.com>
2024-04-29 10:14:15 -0400

    Update disk_dimensions.sh
    Update JSON filtering to gather disk information, formatted to be used directly by Terraform with "data.external.disk_dimensions.result"

--- ec2_alarm/disk_dimensions.sh
+++ ec2_alarm/disk_dimensions.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 #
+# Forked from https://github.com/robin-zhao/terraform-aws-cloudwatch-metrics-for-ec2/blob/master/ec2_alarm/disk_dimensions.sh
+#
 # Example
-#
-# echo '{"profile":"ap","region":"ap-east-1","instance_id":"123"}' | bash disk_dimensions.sh
-#
+# echo '{"profile":"default","region":"us-east-1","instance_id":"123","disk_path":"/"}' | bash disk_dimensions.sh
 
 
 # Exit if any of the intermediate steps fail
 set -e
+eval "$(jq -r '@sh "export PROFILE=\(.profile) REGION=\(.region) DISK_PATH=\(.disk_path) INSTANCE_ID=\(.instance_id)"')"
 
-eval "$(jq -r '@sh "PROFILE=\(.profile) REGION=\(.region) INSTANCE_ID=\(.instance_id)"')"
+
+#function parse_json_input() {
+#    eval "$(jq -r '@sh "PROFILE=\(.profile) REGION=\(.region) DISK_PATH=\(.disk_path) INSTANCE_ID=\(.instance_id)"')"
+#    if [[ "${PROFILE}" == null ]]; then exit 1; fi
+#    if [[ "${REGION}" == null ]]; then exit 1; fi
+#    if [[ "${DISK_PATH}" == null ]]; then exit 1; fi
+#    if [[ "${INSTANCE_ID}" == null ]]; then exit 1; fi
+#}
+#parse_json_input
 
 
 aws cloudwatch list-metrics \
@@ -17,5 +26,6 @@ aws cloudwatch list-metrics \
     --metric-name disk_used_percent \
     --profile $PROFILE \
     --region $REGION \
-    --dimensions Name=InstanceId,Value=$INSTANCE_ID --query "Metrics[0].Dimensions" \
-    | jq 'from_entries'
+    --dimensions Name=InstanceId,Value=$INSTANCE_ID \
+    | jq -r --arg DISK_PATH "$DISK_PATH" '.Metrics[] | select(.Dimensions[].Value==$DISK_PATH)' \
+    | jq -r '.Dimensions' | jq 'from_entries' | jq -c
